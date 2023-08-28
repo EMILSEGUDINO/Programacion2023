3c89e84e1d60a344265f14507059629c0eb612ae
EMILSEGUDINO <114962555+EMILSEGUDINO@users.noreply.github.com>
2023-08-28 20:52:05 -0300

    Add files via upload

---
+++ #SORTEO------------------.py
@@ -0,0 +1,89 @@
+#SORTEO------------------
+
+from traceback import print_stack
+
+
+{num = int (input("Ingrese un nùmero:"))
+ 
+    num= int 
+    
+    if num = 10 
+    print ("Usted ha ganado")
+
+    else:
+
+    num<10
+    print("!Falto un poco¡ Segui participando")
+
+    elif:
+
+    num>10
+    print("!Te pasaste¡ Segui intentando")
+}
+
+#DIAS DE LA SEMANA------------------
+
+Sem1 = int (input("Ingrese un dìa de la semana:"))
+
+{if Sem1= Lunes 
+print ("Lunes maravilloso")
+
+else
+
+Sem2=viernes
+print("Ultimo dìa de trabajo, wiiii")
+
+elif
+
+Sem3=sabado
+print("sabado de flojera")
+}
+
+{ if Sem1= (Martes, Miercoles, Jueves, domingo)
+    print("No es un buen dìa")
+}
+
+#Letras------------------
+
+{num1 = int (input("Ingrese una letra:"))
+    if letra: vocal
+    print ("Es una vocal")
+
+    else:
+
+    letra1 = vocal
+    print("No es vocal")
+}
+
+#ESTRUCTURAS REPETITIVAS------
+
+{
+suma=0
+while(suma=-1):
+ n=int(input("Ingrese: "))
+ suma=suma+n
+ cantidad=cantidad+1
+print("suma: ", suma)
+print("cantidad: ", cantidad)
+}
+
+{
+    C= input ("ingrese letra: ")
+    c= int
+    vocales = ["a","e","i","o","u","A","E","I","O","U"]
+    
+    while c in vocales:
+     print("Es una Vocal")
+    else:
+    print ("Es NO VOCAL:")
+}
+
+{
+  suma=0
+  while (suma=0):
+  n=int(input("Ingrese: "))
+  suma=suma+n
+  cantidad=cantidad+1
+    print("suma: ")
+    print("cantidad: ")
+}
